e7128ab255b63f1249f192eeba40621140e2c338
Julie Adul <julieachola2@gmail.com>
2025-09-02 12:19:28 +0300

    feat: Update server port

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use('/api/symptom', symptomRoutes);
 app.use('/api/payment', paymentRoutes);
 app.use('/api/admin', adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
